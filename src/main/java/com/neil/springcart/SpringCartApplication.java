90b868978427b8f4c049de9183b453d4feb99d78
Neil Khatri <79176897+nkhatri7@users.noreply.github.com>
2023-11-14 10:00:04 +1100

    feat: initialise spring project

---
+++ src/main/java/com/neil/springcart/SpringCartApplication.java
@@ -0,0 +1,13 @@
+package com.neil.springcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCartApplication.class, args);
+	}
+
+}
