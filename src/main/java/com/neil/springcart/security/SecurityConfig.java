5a24d2b649aa501cedbbbc6163926abc67f47a39
Neil Khatri <79176897+nkhatri7@users.noreply.github.com>
2023-11-27 11:57:36 +1100

    feat: add IsAdmin annotation to protect internal routes

--- src/main/java/com/neil/springcart/security/SecurityConfig.java
+++ src/main/java/com/neil/springcart/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +24,7 @@ import java.util.Optional;
  * Security config class to set up security services in the application.
  */
 @Configuration
+@EnableMethodSecurity
 @AllArgsConstructor
 public class SecurityConfig {
     private final CustomerRepository customerRepository;
