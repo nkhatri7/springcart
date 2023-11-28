da025dc90e5551ea636ceae60bd40023d2490a8f
Neil Khatri <79176897+nkhatri7@users.noreply.github.com>
2023-11-28 15:06:42 +1100

    fix: fix admin authorization annotation

--- src/main/java/com/neil/springcart/annotations/IsAdmin.java
+++ src/main/java/com/neil/springcart/annotations/IsAdmin.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasAuthority('ADMIN')")
 public @interface IsAdmin {}
